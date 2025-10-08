2808b10020c70964a575f815d2bc846cfde3a5a4
muchenx2 <muchenx2@illinois.edu>
2025-10-08 00:08:38 -0500

    q2

--- Homework/hw1/q2.py
+++ Homework/hw1/q2.py
@@ -1,21 +1,25 @@
-###Q2: Parameter Server###
-###please implement PS method, using  parameter update&optimizor states all be held in rank0###
+### Q2: Parameter Server###
+### please implement PS method, using  parameter update&optimizor states all be held in rank0###
 
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 import torch
 import torch.distributed as dist
 
+
 def server(params, opt, world):
     # ---- aggregate grads from workers ----
-    flat_grad = _flatten_dense_tensors([p.grad for p in params]).contiguous() #usage: tranfer a list tensor to one 1-D tensor
-    ##here, you should generate one big 1-D tensor containing all parameters to make the transfer process easy
-    agg = flat_grad.clone() #agg as a aggregated counter to record sum gradients
+    # usage: tranfer a list tensor to one 1-D tensor
+    flat_grad = _flatten_dense_tensors([p.grad for p in params]).contiguous()
+    # here, you should generate one big 1-D tensor containing all parameters to make the transfer process easy
+    agg = flat_grad.clone()  # agg as a aggregated counter to record sum gradients
 
-    #                                                                   #
-    #                                                                   #
-    # your code here: receive gradients form worker, and add them to agg#
-    #                                                                   #
-    #                                                                   #
+    assert world == 3
+    for i in range(1, world):
+        recv_buf = torch.empty_like(agg)
+        r = dist.irecv(recv_buf, src=i)
+        r.wait()
+        agg += recv_buf
+    agg /= world
 
     synced_grads = _unflatten_dense_tensors(agg, [p.grad for p in params])
     # ---- set averaged grads locally & step ----
@@ -25,37 +29,31 @@ def server(params, opt, world):
 
     # ---- broadcast updated params for this subset ----
     flat_param = _flatten_dense_tensors([p.data for p in params]).contiguous()
-    #                                                                   #
-    #                                                                   #
-    # your code here: send packed 1-D parameter tensor to all workers   #
-    #                                                                   #
-    #                                                                   #
+    for i in range(1, world):
+        s = dist.isend(flat_param, dst=i)
+        s.wait()
 
 def worker(params):
     flat_grad = _flatten_dense_tensors([p.grad for p in params]).contiguous()
     # ---- push grads to server ----
 
-    #                                                                   #
-    #                                                                   #
-    # your code here: send packed 1-D gradient to server
-    #                                                                   #
-    #                                                                   #
+    s = dist.isend(flat_grad, dst=0)
+    s.wait()
 
     # ---- receive updated params, write into local model ----
-    
-    #                                                                   #
-    #                                                                   #
-    # your code here: please get correct 1-D packed parameter from server
-    #           And then unpacked it and store in synced_params
-    #                                                                   #
-    synced_params = None #you should  assign correct value for synced_params#
 
+    flat_param_template = _flatten_dense_tensors([p.data for p in params]).contiguous()
+    r = dist.irecv(flat_param_template, src=0)
+    r.wait()
+    
+    synced_params = _unflatten_dense_tensors(flat_param_template, [p.data for p in params])
 
     # ---- syncronize the parameters ----
     for p, s in zip(params, synced_params):
         p.data.copy_(s)
 
-def PS_grads_(model,world_size=None, rankid=None, opt=None):
+
+def PS_grads_(model, world_size=None, rankid=None, opt=None):
     """
     Synchronous PS step:
       - Rank 0: receive grads, sum/avg, set grads, opt.step(), broadcast updated params.
@@ -63,14 +61,14 @@ def PS_grads_(model,world_size=None, rankid=None, opt=None):
     Only processes the subset of parameters with non-None grads.
     """
     world = world_size
-    rank  = rankid
-    
+    rank = rankid
+
     # Fast path: single process
     if world == 1:
         opt.step()
         return
 
-    #not necessary, as in most case, params won't be empty list...
+    # not necessary, as in most case, params won't be empty list...
     # Collect params that participated in this backward pass
     params = [p for p in model.parameters() if p.grad is not None]
     if not params:
